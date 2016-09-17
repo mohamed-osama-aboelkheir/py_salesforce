532f4136ca2c5d47afa9a060d664dd33d6722768
U-CORP\aboelm1 <aboelm1@EGCSABOELM1L1C.corp.emc.com>
2016-09-17 21:46:43 +0200

    * version 0.2.3

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ setup(
   package_data={
     'py_salesforce': ['py_salesforce.conf'],
   },
-  version = '0.2.2',
+  version = '0.2.3',
   description = ' A Python wrapper for the Salesforce REST API and SOQL.',
   author = 'Mohamed AboElKheir',
   author_email = 'mohamed.osama.aboelkheir@gmail.com',
   url = 'https://github.com/mohamed-osama-aboelkheir/py_salesforce', 
-  download_url = 'https://github.com/mohamed-osama-aboelkheir/py_salesforce/tarball/0.2.2', 
+  download_url = 'https://github.com/mohamed-osama-aboelkheir/py_salesforce/tarball/0.2.3', 
   keywords = ['salesforce', 'REST', 'API','SOQL'], 
   install_requires=['httplib2'],
   classifiers = [
