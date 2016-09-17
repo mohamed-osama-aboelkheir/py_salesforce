bb3cbb69a9de16f6064adee33406cbafb12c5dcb
U-CORP\aboelm1 <aboelm1@EGCSABOELM1L1C.corp.emc.com>
2016-09-17 18:48:19 +0200

    * prepared as pip package

---
+++ py_salesforce/__init__.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python
+#
+# DESCRIPTION:	
+# AUTHOR: 	Mohamed Osama (mohamed.osama.aboelkheir@gmail.com)
+# CREATED: 	Sat 17-Sep-2016
+# LAST REVISED:	Sat 17-Sep-2016
+#
+##############
+# DISCLAIMER #
+##############
+# Anyone is free to copy, modify, use, or distribute this script for any purpose, and by any means. However, Please take care THIS IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND AND YOU SHOULD USE IT AT YOUR OWN RISK.
+
+from py_salesforce import py_salesforce
